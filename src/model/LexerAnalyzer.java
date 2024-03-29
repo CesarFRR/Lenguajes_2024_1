973013e4e3a1a1e368b76d98276d427b2a6567fe
CesarFRR <crinconro@unal.edu.co>
2024-03-29 12:07:24 -0500

    Trying to use lexer analyzer

--- src/model/LexerAnalyzer.java
+++ src/model/LexerAnalyzer.java
@@ -4,10 +4,17 @@
  */
 package model;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.Reader;
 import model.Lexer;
+import model.Token;
 public class LexerAnalyzer {
 
     private Lexer lexer;
@@ -17,14 +24,27 @@ public class LexerAnalyzer {
     }
 
     public static void main(String[] args) throws IOException {
-        // Usar el archivo ex1.txt en la carpeta data
-        LexerAnalyzer lexerAnalyzer = new LexerAnalyzer("../data/ex1.txt");
-        Token token;
-        do {
-            token = lexerAnalyzer.lexer.next_token();
-            if (token != null) {
-                System.out.println("Token: " + token.text + ", Tipo: " + token.type);
+        
+        // Extraer tokens
+        Lexer lexer;
+        try {
+            File codigo = new File("../data/ex1.txt");
+            FileOutputStream output = new FileOutputStream(codigo);
+            byte[] bytesText = jtpCode.getText().getBytes();
+            output.write(bytesText);
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(new FileInputStream(codigo), "UTF8"));
+            lexer = new Lexer(entrada);
+            while (true) {
+                Token token = lexer.yylex();
+                if (token == null) {
+                    break;
+                }
+                tokens.add(token);
             }
-        } while (token.type != TokenType.EOF); // Continuar hasta que next_token() devuelva null, lo que indica el fin de archivo
+        } catch (FileNotFoundException ex) {
+            System.out.println("El archivo no pudo ser encontrado... " + ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println("Error al escribir en el archivo... " + ex.getMessage());
+        }
     }
 }
